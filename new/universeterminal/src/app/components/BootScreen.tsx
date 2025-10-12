8cf1693170af081209bdc78d3aa9918aa1155e23
Justin Lee <leejustincs@gmail.com>
2025-10-12 16:41:36 -0700

    feat: add temporary boot screen
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- new/universeterminal/src/app/components/BootScreen.tsx
+++ new/universeterminal/src/app/components/BootScreen.tsx
@@ -1,7 +1,18 @@
-export default function BootScreen() {
-    return(
-        <div>
-            <h1>Boot Screen</h1>
-        </div>
-    )
+import { useEffect } from 'react';
+
+export default function BootScreen({ onNext }: { onNext: () => void }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onNext();
+    }, 3000); // length of your animation in ms
+
+    return () => clearTimeout(timer);
+  }, [onNext]);
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-black text-bone">
+      {/* Boot animation visuals go here */}
+      <p>Booting...</p>
+    </div>
+  );
 }
