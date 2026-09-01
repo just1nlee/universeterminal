3417812a84239d4f3f17465a32827a443427ffa6
Justin Lee <leejustincs@gmail.com>
2026-09-01 15:13:29 -0700

    Clean up before next subtree merge
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- universeterminalv1/frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/src/app/page\": [\n      \"static/media/pxiKyp0ihIEF2isfFJXUdVNF-s.p.6cea96a1.woff2\",\n      \"static/media/e3t4euO8T_267oIAQAu6jDQyK3nVivNm4I81-s.p.f1e8f2e8.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
